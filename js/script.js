6608b5e81738496b1254bc344e7df704f5f76a74
Mamed <mamed.saliev.90@list.ru>
2021-12-19 23:03:05 +0500

    comments

--- js/script.js
+++ js/script.js
@@ -8,4 +8,28 @@ $(document).ready(function () {
       prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-right.png"></button>',
       nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-left.png"></button>'
    });
+   // $('.js-open').each(function (i) {
+   //    $(this).on("click", function (e) {
+   //       e.preventDefault()
+   //       $('.product__content').eq(i).addClass('product__content_active')
+   //    });
+   // });
+
+   // $('.js-close').each(function (i) {  //  i- индекс элементво 0-1-2....
+   //    $(this).on("click", function (e) {
+   //       e.preventDefault()  ///  убирает действие по умолчанию 
+   //       $('.product__content').eq(i).removeClass('product__content_active')
+   //    });
+   // })
+   function toggleOpen(item) {
+      $(item).each(function (i) {  //  i- индекс элементво 0-1-2....
+         $(this).on("click", function (e) {
+            e.preventDefault()  ///  убирает действие по умолчанию 
+            $('.product__content').eq(i).toggleClass('product__content_active')
+         });
+      });
+   }
+   toggleOpen(".js-open");
+   toggleOpen(".js-close")
+
 })
